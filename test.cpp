736070d2d00af1871e595770b6481028af0221d9
AMIRHOSEIN_NOURI <amirhoseinnouri5644@gmail.com>
2025-03-28 13:26:17 +0330

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
-using namespace std ;
-
-class car {
-    private : 
-    string brand;
-    string model;
-    int year;
-
-    public:
-    car(string b , string m , int y){
-        brand = b ;
-        model = m;
-        year = y ;
-
+using namespace std;
+class BubbleSort {
+private:
+    int* arr;
+    int size;
+public:
+    BubbleSort(int* array, int n) : arr(array), size(n) {}
+    void display() const {
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
-    void display (){
-        cout<<"brand"<<brand<<",model"<<model<<",year"<<year<<endl;
-
+    void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            bool swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+    int* getSortedArray() const {
+        return arr;
     }
 };
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-int main(){
-    car mycar("toyota" , "corolla" , 2022);
-
-    mycar.display();
-    return 0 ;
-    
+    cout << "array before sort : ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    BubbleSort bubbleSorter(arr, n);
+    bubbleSorter.sort();
+    cout << "array after sort : ";
+    bubbleSorter.display();
 }
